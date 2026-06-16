831a8f08eae449acce07194751375b043d9711f7
Easyprogrammer <531302574@qq.com>
2026-06-16 13:27:02 +0800

    Use same-origin API when self-hosted

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,7 @@
-window.TYCA_TOOL_CONFIG = {
-  apiBase: "https://47.106.167.94.sslip.io"
-};
+(function () {
+  var apiBase = window.location.origin;
+  if (window.location.hostname === "easyprogrammer.github.io") {
+    apiBase = "https://47.106.167.94.sslip.io";
+  }
+  window.TYCA_TOOL_CONFIG = { apiBase: apiBase };
+})();
